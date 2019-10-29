a5da3fa23127065179b9785e0db40b3543362d4d
annabeatrizlucena <anna.lira@ccc.ufc.edu.br>
2019-10-29 17:26:01 -0300

    Adicionando equals e hashCode em Pesquisa

--- ProjetoLP2/src/pacote/Pesquisa.java
+++ ProjetoLP2/src/pacote/Pesquisa.java
@@ -50,4 +50,30 @@ public class Pesquisa {
 	public String toString() {
 		return this.codigo + " - " + this.descricao + " - " + this.campoDeInteresse;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pesquisa other = (Pesquisa) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
 }
---
+++ ProjetoLP2/testes_Unidade/pacote/PesquisadorTest.java
@@ -0,0 +1,14 @@
+package pacote;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PesquisadorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
