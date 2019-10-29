768282104c17bd39116907a826eda5a8efb5efaf
119110382 ANNA BEATRIZ LUCENA LIRA <annabll@lcc3-073.lcc.ufcg.edu.br>
2019-10-29 08:23:58 -0300

    Adicionando o Junit

---
+++ ProjetoLP2/testes_Unidade/pacote/ControllerPesquisadorTest.java
@@ -0,0 +1,14 @@
+package pacote;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerPesquisadorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
