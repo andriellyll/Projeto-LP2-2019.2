3f2075b42c25b7fa2b474be940ef25aed3310229
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2019-10-29 13:42:01 -0300

    Testes de unidade da US3 e corrigi alguns erros da minha US

--- ProjetoLP2/testes_Unidade/pacote/ControllerProblemaObjetivoTest.java
+++ ProjetoLP2/testes_Unidade/pacote/ControllerProblemaObjetivoTest.java
@@ -103,24 +103,111 @@ public class ControllerProblemaObjetivoTest {
 		});
 	}
 	
+	@Test
+	public void testApagarProblemaNull() {
+		assertThrows(NullPointerException.class, () -> {
+			controllerProbObj.exibeProblema(null);
+		});
+	}
+	
+	@Test
+	public void testApagarProblemaVazio() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.exibeProblema("");
+		});
+	}
+	
+	@Test
+	public void testApagarProblemaInexistente() {
+		String codigo = controllerProbObj.cadastraProblema("O problema do discurso homofobico em chats online de alunos de computacao de primeiro periodo", 2);
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.exibeProblema("O2");
+		});
+	}
+
+	@Test
+	public void testApagarObjetivo() {
+		String codigo = controllerProbObj.cadastraObjetivo("GERAL", "Diminuir a frequencia de mensagens homofobicas trocadas em chats online entre alunos de primeiro periodo de computacao.", 4, 2);
+		controllerProbObj.exibeObjetivo(codigo);
+		controllerProbObj.apagarObjetivo(codigo);
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.exibeObjetivo(codigo);	
+		});
+	}
+	
+	@Test
+	public void testApagarObjetivoNull() {
+		assertThrows(NullPointerException.class, () -> {
+			controllerProbObj.apagarObjetivo(null);
+		});
+	}
+	
+	@Test
+	public void testApagarObjetivoVazio() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.apagarObjetivo("");
+		});
+	}
+	
+	@Test
+	public void testApagarObjetivoInexistente() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.apagarObjetivo("O1");
+		});
+	}
+	@Test
+	public void testExibeProblema() {
+		String codigo = controllerProbObj.cadastraProblema("O problema do discurso homofobico em chats online de alunos de computacao de primeiro periodo", 2);
+		assertEquals(codigo + " - O problema do discurso homofobico em chats online de alunos de computacao de primeiro periodo - 2", controllerProbObj.exibeProblema(codigo));
+	}
+
+	
+
+	@Test
+	public void testExibeProblemaNull() {
+		assertThrows(NullPointerException.class, () -> {
+			controllerProbObj.exibeProblema(null);
+		});
+	}
+	
+	@Test
+	public void testExibeProblemaVazio() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.exibeProblema("");
+		});
+	}
+	
+	@Test
+	public void testExibeProblemaInexistente() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.exibeProblema("O1");
+		});
+	}
+	
+	@Test
+	public void testExibeObjetivo() {
+		String codigo = controllerProbObj.cadastraObjetivo("GERAL", "Diminuir a frequencia de mensagens homofobicas trocadas em chats online entre alunos de primeiro periodo de computacao.", 4, 2);
+		assertEquals(codigo + " - GERAL - Diminuir a frequencia de mensagens homofobicas trocadas em chats online entre alunos de primeiro periodo de computacao. - 6", controllerProbObj.exibeObjetivo(codigo));
+	}
+	
+	@Test
+	public void testExibeObjetivoNull() {
+		assertThrows(NullPointerException.class, () -> {
+			controllerProbObj.exibeObjetivo(null);
+		});
+	}
+	
+	@Test
+	public void testExibeObjetivoVazio() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.exibeObjetivo("");
+		});
+	}
+	
+	@Test
+	public void testExibeObjetivoInexistente() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			controllerProbObj.exibeObjetivo("O1");
+		});
+	}
 }
-/*
-
-
-	@Test
-	void testApagarObjetivo() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testExibeProblema() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testExibeObjetivo() {
-		fail("Not yet implemented");
-	}
-
-}
-*/
--- ProjetoLP2/testes_Unidade/pacote/ProblemaTest.java
+++ ProjetoLP2/testes_Unidade/pacote/ProblemaTest.java
@@ -7,6 +7,21 @@ import org.junit.jupiter.api.Test;
 public class ProblemaTest {
 
 	@Test
+	public void testProblemaNull() {
+		
+	}
+	
+	@Test
+	public void testProblemaVazio() {
+		
+	}
+	
+	@Test
+	public void testProblemaViabilidadeInvalida() {
+		
+	}
+	
+	@Test
 	public void testToString() {
 		Problema problema = new Problema("A dificuldade da predicao do sistema eleitoral brasileiro", 1, "P1");
 		assertEquals("P1 - A dificuldade da predicao do sistema eleitoral brasileiro - 1", problema.toString());
