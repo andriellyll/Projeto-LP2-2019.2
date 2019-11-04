1abf4d1589ccb624a705c633309a2a571ca718af
andriellyll <andrielly.lucena@ccc.ufcg.edu.br>
2019-11-04 17:15:21 -0300

    Terminei as funcionalidades da US8

--- ProjetoLP2/src/pacote/Pesquisa.java
+++ ProjetoLP2/src/pacote/Pesquisa.java
@@ -306,8 +306,11 @@ public class Pesquisa {
 	 */
 	public List<String> procuraPalavraChave(String palavraChave) {
 		ArrayList<String> resultadosBusca = new ArrayList<>();
-		if (this.descricao.contains(palavraChave) || this.campoDeInteresse.contains(palavraChave)) {
+		if (this.descricao.contains(palavraChave)) {
 			resultadosBusca.add(this.codigo + ": " + this.descricao);
+		}
+		if(this.campoDeInteresse.contains(palavraChave)) {
+			resultadosBusca.add(this.codigo + ": " + this.descricao);	
 		}
 		return resultadosBusca;
 	}
