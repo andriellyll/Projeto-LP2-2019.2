26cc8c2d93a50268398addcbfa3d9da29d4ae858
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2019-11-09 10:53:13 -0300

    Casos de teste atualizados

--- ProjetoLP2/src/utils/OrdenaResultados.java
+++ ProjetoLP2/src/utils/OrdenaResultados.java
@@ -6,9 +6,18 @@ public class OrdenaResultados implements Comparator<String>{
 
 	@Override
 	public int compare(String str1, String str2) {
-		String partStr1 = str1.toLowerCase();
-		String partStr2 = str2.toLowerCase();
-		return partStr2.compareTo(partStr1);
+
+		String partStr1 = str1.split(":")[0];
+		String partStr2 = str2.split(":")[0];
+		/*
+		String otaPartStr1 = str1.split(":")[1].toLowerCase();
+		String otaPartStr2 = str2.split(":")[1].toLowerCase();
+
+		if(partStr2.compareTo(partStr1) != 0) {
+			return partStr2.compareTo(partStr1);
+		}
+		*/
+		return partStr2.toLowerCase().compareTo(partStr1.toLowerCase());
 	}
 
 }
