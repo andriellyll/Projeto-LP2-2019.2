a25e9d4af8075c15adc7c27af4b897b213302444
Anna Beatriz Lucena <anna.lira@ccc.ufcg.edu.br>
2019-11-15 16:59:58 -0300

    ignorar

--- ProjetoLP2/src/pacote/Facade.java
+++ ProjetoLP2/src/pacote/Facade.java
@@ -20,10 +20,10 @@ public class Facade {
 	}
 
 	public static void main(String[] args) {
-		args = new String[] { "pacote.Facade", "testes_aceitacao/use_case_1.txt", "testes_aceitacao/use_case_2.txt",
+		args = new String[] { "pacote.Facade","testes_aceitacao/use_case_1.txt", "testes_aceitacao/use_case_2.txt",
 				"testes_aceitacao/use_case_3.txt", "testes_aceitacao/use_case_4.txt", "testes_aceitacao/use_case_5.txt",
 				"testes_aceitacao/use_case_6.txt", "testes_aceitacao/use_case_7.txt",
-				"testes_aceitacao/use_case_8.txt"};
+				"testes_aceitacao/use_case_8.txt" };
 		EasyAccept.main(args);
 	}
 
