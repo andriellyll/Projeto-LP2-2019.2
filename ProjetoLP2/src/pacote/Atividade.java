d4e0cf9524e9e2140b13c1f7618aeb9ec8505a26
Henrique Lemos <henrique.leite@ccc.ufcg.edu.br>
2019-11-18 16:47:55 -0300

    Conclusão da US9 (Oficial)

--- ProjetoLP2/src/pacote/Atividade.java
+++ ProjetoLP2/src/pacote/Atividade.java
@@ -463,11 +463,17 @@ public class Atividade implements Serializable, Comparable<Atividade> {
 	 * 
 	 * @return
 	 */
-	public String atividadeMaiorRisco(String maiorRisco) {
+	public String atividadeMaiorRisco(String partida) {
 		if (this.seguinteNaCadeia == null) {
 			throw new RuntimeException("Nao existe proxima atividade.");
 		}
-		return maiorRisco(maiorRisco);
+		if (maiorRisco(partida, "ALTO") == null) {
+			if (maiorRisco(partida, "MEDIO") == null) {
+				return maiorRisco(partida, "BAIXO");
+			}
+			return maiorRisco(partida, "MEDIO");
+		}
+		return maiorRisco(partida, "ALTO");
 	}
 	
 	/**
@@ -476,18 +482,28 @@ public class Atividade implements Serializable, Comparable<Atividade> {
 	 * @param maior
 	 * @return
 	 */
-	private String maiorRisco(String maior) {
+	private String maiorRisco(String maior, String classificacao) {
 		String maiorRiscoNaCadeia = maior;
-		if ("ALTO".equals(this.nivelRisco)) {
-			maiorRiscoNaCadeia = this.codigo;
-		}
+//		maior risco {
+//			if maiorRiscoRecursivo("ALTO") == null
+//					if maiorRiscoRecursivo("MEDIO") == null
+//							maiorRiscoRecursivo("BAIXO")
+//			
+//							
+//			return maiorRiscoRecursivo("ALTO");
+//		}
+//		if (classificacao.equals(this.nivelRisco)) {
+//			maiorRiscoNaCadeia = this.codigo;
+//		}
 		if (this.seguinteNaCadeia == null) {
 			return maiorRiscoNaCadeia;
-		}
-		if ("ALTO".equals(this.seguinteNaCadeia.nivelRisco)) {
+			}
+		if (classificacao.equals(this.seguinteNaCadeia.nivelRisco)) {
 			maiorRiscoNaCadeia = this.seguinteNaCadeia.codigo;
 		}
-		return this.seguinteNaCadeia.maiorRisco(maiorRiscoNaCadeia);
+		return this.seguinteNaCadeia.maiorRisco(maiorRiscoNaCadeia, classificacao);
+		
+		
 	}
 	
 	/**
